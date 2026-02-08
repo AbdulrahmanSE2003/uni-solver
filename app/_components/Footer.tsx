d8e269b22110e84b6ddf9eb6726cfdcbb98e6902
Abdulrahman <abdulrahman.saad2303@gmail.com>
2026-02-09 01:04:28 +0200

    smallen the footer font

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <footer className="flex justify-between items-center bg-brand-bg p-8 px-12">
             <Logo />
-            <p className="text-zinc-600 capitalize"> &copy; {new Date().getFullYear()} UniSolver &mdash; All Rights reserved {" "}
+            <p className="text-zinc-600 capitalize text-sm"> &copy; {new Date().getFullYear()} UniSolver &mdash; All Rights reserved {" "}
                 <a className="hover:text-brand-text transition-colors duration-300 underline underline-offset-2" href="https://mnmlst-nine.vercel.app/" target="_blank">Mnmlst</a>
             </p>
         </footer>
