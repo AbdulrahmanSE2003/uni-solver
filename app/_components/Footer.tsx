fc16da67c16ce6379a32e9e71b18434e1689281d
Abdulrahman <abdulrahman.saad2303@gmail.com>
2026-02-21 14:59:14 +0200

    handled footer responsive

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -1,12 +1,21 @@
 import Logo from "./Logo";
 
 export default function Footer() {
-    return (
-        <footer className="flex justify-between items-center bg-brand-bg p-8 px-12">
-            <Logo />
-            <p className="text-zinc-600 capitalize text-sm"> &copy; {new Date().getFullYear()} UniSolver &mdash; All Rights reserved for {" "}
-                <a className="hover:text-brand-text transition-colors duration-300 underline underline-offset-2" href="https://mnmlst-nine.vercel.app/" target="_blank">Mnmlst</a>
-            </p>
-        </footer>
-    )
+  return (
+    <footer className="flex max-md:flex-col max-md:gap-5 max-md:items-start md:justify-between items-center bg-brand-bg p-8 px-12">
+      <Logo />
+      <p className="text-zinc-600 capitalize text-sm">
+        {" "}
+        &copy; {new Date().getFullYear()} UniSolver &mdash; All Rights reserved
+        for{" "}
+        <a
+          className="hover:text-brand-text transition-colors duration-300 underline underline-offset-2"
+          href="https://mnmlst-nine.vercel.app/"
+          target="_blank"
+        >
+          Mnmlst
+        </a>
+      </p>
+    </footer>
+  );
 }
